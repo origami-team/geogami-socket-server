aa510cc8c2dddf86ccdd0e9129429400e0266db9
Yousef Qamaz <40634685+YouQam@users.noreply.github.com>
2022-12-06 22:20:17 +0100

    store and check game stored status

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ let io = require('socket.io')(process.env.PORT || 3005);
 // Rooms impl.
 //const { makeid } = require('./utils');
 const clientRooms = {};
+const isGameTrackStored = {}
 const roomVRWorldType = {};
 
 console.log("Server Started");
@@ -17,6 +18,8 @@ io.on('connection', async (socket) => {
   // Functions' declaration
   // socket.on('newGame', handleNewGame);
   socket.on('joinGame', handleJoinGame);
+  socket.on('updateGameTrackStauts', handleUpdateGameTrackStauts);
+  // socket.on('checkIsGameTrackStored', handleCheckIsGameTrackStored);
   socket.on('updateAvatarPosition', handleUpdateAvatarPosition);
   socket.on('updateAvatarDirection', handleUpdateAvatarDirection);
   socket.on('checkRoomExistance', handleCheckRoomExistance);
@@ -24,42 +27,78 @@ io.on('connection', async (socket) => {
   /* new impl (multiplayer-realworld) */
   // socket.on('assignPlayerNumber', handleAssignPlayerNumber);
 
+  // add to hlep function
+  /*   function checkNumberOfPlayers(roomName) {
+    } */
+
   // Functions' definitions
   function handleJoinGame(gameCodeRecieved) {
     console.log("/// gameCodeRecieved: ", gameCodeRecieved);
     // Assign received gamecode to a var.
     let roomName = gameCodeRecieved["gameCode"];
+    let gameNumPlayers = gameCodeRecieved["gameNumPlayers"];
+    let playersCount;
 
-    // let isVRWrorld_1 = gameCodeRecieved["isVRWorld_1"]; // No need for it now, until we use multiplayer in VE
+    /* check whether game can accept further players */
+    // check whether room exist
+    if (io.sockets.adapter.rooms[roomName]) {
+      // Get players count in room
+      // playerNo equal the current length of users in the room
+      playersCount = io.sockets.adapter.rooms[roomName].length;
+      //check if game still accept more players
+      // if not send user notification
+      if (playersCount >= gameNumPlayers) {
+        io.to(socket.id).emit('gamePlayersFull', { msg: `Sorry this game accepts only ${gameNumPlayers} players.` })
+        console.log("don't allow adding player")
+        return;
+      }
+    } else {
+      // Enterd only when room is empty
+      playersCount = 1;
+      // Initialize track stored status to false
+      isGameTrackStored[roomName] = { status: false, game_id: undefined };
+      console.log("isGameTrackStored: ", isGameTrackStored);
+    }
 
-    /* Assign room name to ceated object */
+    /* Assign room name to local object */
     clientRooms[socket.id] = roomName;
-    // roomVRWorldType[roomName] = isVRWrorld_1; // to send the VR world type in `checkRoomExistance`
 
-    /* get user id to specify msg to users */
-    // const userId = await fetchUserId(socket);
-    console.log("// socket.id: ", socket.id);
-
-    //TODO: send name to frontend
     /* Join player to room */
     socket.join(roomName);
 
-    /* send playerNo to user */
-    // playerNo equal the current length of users in the room
-    let playerNum = io.sockets.adapter.rooms[roomName].length;
-    // send playerNo to the user using socket ID
-    io.to(socket.id).emit('assignPlayerNumber', { playerNo: playerNum, playerID: socket.id })
+    /* send playerNo to user using socket ID */
+    io.to(socket.id).emit('assignPlayerNumber', { playerNo: playersCount, playerID: socket.id })
 
-    /* Notify all players of number of enrolled player (to be able to start game wen all are in) */
-    socket.to(roomName).emit('PlayerJoined', { joinedPlayersCount: playerNum })
-
-
-    const room = io.sockets.adapter.rooms[roomName];
-    console.log("room: ", room);
+    /* Notify all players of number of joined players except joined member (to be able to start game wen all are in) */
+    socket.to(roomName).emit('playerJoined', { joinedPlayersCount: playersCount })
 
     // temp
     printNumRoomMembers(roomName); //Print number of members
   }
+
+  /* multiplayer */
+  function handleUpdateGameTrackStauts(data) {
+    let roomName = data["roomName"]
+    let game_id = data["game_id"]
+
+    console.log("name: ", roomName, "///// id: ", game_id)
+    isGameTrackStored[roomName] = { status: true, game_id: game_id };
+  }
+
+  /* multiplayer */
+  // Check whether game is already stored by one of the players
+  socket.on('checkIsGameTrackStored', (roomName, callback) => {
+    let trackStoredStatus = isGameTrackStored[roomName];
+    console.log("roomName: ", roomName);
+    console.log("trackStoredStatus: ", trackStoredStatus);
+
+    callback({
+      status: trackStoredStatus["status"]
+    });
+
+    /* send game track stutus to sender */
+    // io.to(socket.id).emit('isGameTrackStored', { trackStoredStatus: trackStoredStatus["status"] })
+  });
 
   function handleUpdateAvatarPosition(avatarPosition) {
     console.log("Loc", { x: avatarPosition["x_axis"], z: avatarPosition["y_axis"], r_code: avatarPosition["gameCode"] });
