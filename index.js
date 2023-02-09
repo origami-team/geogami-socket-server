f2c874099fce49975096cb3290d7dbcc9b9079f5
Yousef Qamaz <40634685+YouQam@users.noreply.github.com>
2023-02-09 16:09:46 +0100

    code refactoring

--- index.js
+++ index.js
@@ -12,11 +12,13 @@ const roomVRWorldType = {};
 
 console.log("Server Started");
 
+/*********/
+/* connection */
 io.on('connection', socket => {
   // Print
   console.log("Connection made!!!");
-  //console.log("socket.id", socket.id);
-  // Functions' declaration
+  
+  /* Functions' declaration */
   socket.on('newGame', handleNewGame);
   socket.on('joinGame', handleJoinGame);
   socket.on('updateAvatarPosition', handleUpdateAvatarPosition);
@@ -24,6 +26,8 @@ io.on('connection', socket => {
   socket.on('checkRoomExistance', handleCheckRoomExistance);
 
   // Functions' definitions
+  /****************************************/
+  /* step 1: join game using geogmai App  */
   function handleNewGame(gameCodeRecieved) {
     //let roomName = makeid(5);
     let roomName = gameCodeRecieved["gameCode"];
@@ -36,6 +40,23 @@ io.on('connection', socket => {
     printNumRoomMembers(roomName); //Print number of members
   }
 
+  /*******************************************************************************/
+  /* step 2:  V.E. check if room exists (game is opend using geogmai App) or not */
+  function handleCheckRoomExistance(gameCodeRecieved) {
+    let roomCode = gameCodeRecieved["gameCode"];
+    // Check if room is created
+    if (io.sockets.adapter.rooms[roomCode]) {
+      console.log("Info: Room exist!!");
+      /* send back room code and V.E. type */
+      io.emit('checkRoomExistance', { roomCode: roomCode, roomStatus: true, roomVRWorldType: roomVRWorldType[roomCode] })
+    } else {
+      console.log("Warning: Room doesn't exist!!!??");
+      io.emit('checkRoomExistance', { roomCode: roomCode, roomStatus: false })
+    }
+  }
+
+  /****************************************/
+  /* step 3: to join game using V.E. App  */
   function handleJoinGame(roomNameObj) {
     let roomName = roomNameObj["gameCode"];
     console.log("roomName obj: ", roomNameObj);
@@ -51,37 +72,31 @@ io.on('connection', socket => {
     /* } else {
       console.log("Warning: Room doesn't exist!!!");
     } */
-    
     //ToDO: add notificatoin that a new user has been joined
   }
 
+  /*********/
+  /* Avatar position */
   function handleUpdateAvatarPosition(avatarPosition) {
-    console.log("Loc", { x: avatarPosition["x_axis"], z: avatarPosition["y_axis"], r_code: avatarPosition["gameCode"] });
+    // console.log("Loc", { x: avatarPosition["x_axis"], z: avatarPosition["y_axis"], r_code: avatarPosition["gameCode"] });
     io.to(avatarPosition["gameCode"]).emit('updateAvatarPosition', { x: avatarPosition["x_axis"], z: avatarPosition["y_axis"] })
   }
 
+  /*********/
+  /* Avatar direction */
   function handleUpdateAvatarDirection(avatarHeading) {
-    console.log("Direction", {angleValue: avatarHeading["x_axis"], r_code: avatarHeading["gameCode"] });
-    io.to(avatarHeading["gameCode"]).emit('updateAvatarDirection', {angleValue: avatarHeading["x_axis"]})  
+    // console.log("Direction", {angleValue: avatarHeading["x_axis"], r_code: avatarHeading["gameCode"] });
+    io.to(avatarHeading["gameCode"]).emit('updateAvatarDirection', { angleValue: avatarHeading["x_axis"] })
   }
 
-  function handleCheckRoomExistance(gameCodeRecieved) {
-    let roomCode = gameCodeRecieved["gameCode"];
-    // Check if room is created
-    if(io.sockets.adapter.rooms[roomCode]){
-      console.log("Info: Room exist!!");
-      io.emit('checkRoomExistance', {roomCode: roomCode ,roomStatus: true, roomVRWorldType: roomVRWorldType[roomCode]})
-    } else {
-      console.log("Warning: Room doesn't exist!!!??");
-      io.emit('checkRoomExistance', {roomCode: roomCode ,roomStatus: false})
-    }
-  }
-
-  // Helping functions
-  function printNumRoomMembers(roomName){
+  /*********************/
+  /* Helping functions */
+  function printNumRoomMembers(roomName) {
     console.log("Num Room Members(new): ", io.sockets.adapter.rooms[roomName].length);
   }
 
+  /*********************/
+  /* disconnect */
   socket.on('disconnect', function () {
     console.log("Disonnection!!");
   });
